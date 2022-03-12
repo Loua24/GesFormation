cf40a1b8812b33d97851cc68009cc7e78ea72e86
nessr <nessr@LAPTOP-JQNFS84P>
2022-03-12 09:54:37 +0100

    pom update for jacoco

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,9 @@
-FROM openjdk:11 
-EXPOSE 8080
-ADD target/GesF-1.2-SNAPSHOT.jar GesF-1.2-SNAPSHOT.jar
-ENTRYPOINT ["java","-jar","/GesF-1.2-SNAPSHOT.jar"]
+FROM maven:3.8.1-jdk-11
+
+WORKDIR /GesFormation
+
+COPY . .
+
+RUN mvn clean install -Dmaven.test.skip=true
+
+CMD mvn spring-boot:run
--- src/test/java/com/esprit/examen/services/CoursServiceTest.java
+++ src/test/java/com/esprit/examen/services/CoursServiceTest.java
@@ -3,6 +3,8 @@ package com.esprit.examen.services;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
